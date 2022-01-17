7ba3b1e011ef5fe5ef0ff5e43fba32eaf812ef5b
Hugo-Georges <lafuente.hg@gmail.com>
2022-01-17 17:25:06 +0100

    version du 17-01-2022

--- database/migrations/2022_01_14_080256_create_comments_table.php
+++ database/migrations/2022_01_14_080256_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,11 @@ class CreateCarsTable extends Migration
      */
     public function up()
     {
-        Schema::create('cars', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('marque');
-            $table->string('modele');
-            $table->integer('prix');
-            $table->unsignedBigInteger('comments_id');
-            $table->foreign('comments_id')->references('id')->on('comments');
+            $table->string('contenu');
             $table->timestamps();
-
         });
-
     }
 
     /**
@@ -33,6 +27,6 @@ class CreateCarsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cars');
+        Schema::dropIfExists('comments');
     }
 }
