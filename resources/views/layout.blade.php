ef1f48f82c2fc62841e72b6450abab1aa09eba62
Hugo-Georges <lafuente.hg@gmail.com>
2022-01-28 10:03:44 +0100

    version du 28-01-2022

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -33,7 +33,15 @@
                 </form>
                 <div class="navbar-nav">
                     <div class="nav-item text-nowrap">
-                        <a class="nav-link px-3" href="{{ route('logout') }}">Log out</a>
+                        <a  class="nav-link px-3" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                     </div>
                 </div>
             </header>
